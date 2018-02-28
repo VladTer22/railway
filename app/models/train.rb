338878da1afe94404a5c310560dce5bfe94d8d9a
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-02-28 12:11:18 +0100

    Added migration for routes table

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,6 @@
 class Train < ApplicationRecord
+
+  belongs_to :route
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_153009) do
+ActiveRecord::Schema.define(version: 2021_02_23_085735) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -24,10 +29,17 @@ ActiveRecord::Schema.define(version: 2021_02_22_153009) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "route_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
 end
