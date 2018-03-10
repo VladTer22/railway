b95dd313f8ced9fb31029ebcb5546b70e38aa09a
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-03-10 12:11:18 +0100

    Added station display for routes

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -50,6 +50,6 @@ class TrainsController < ApplicationController
   end
 
   def train_params
-    params.require(:train).permit(:number, :route_id)
+    params.require(:train).permit(:number, :route_id, :current_station_id)
   end
 end
