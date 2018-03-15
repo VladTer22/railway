b771eae9a7ed1beb7eb49cf4b0372e3942c9d2c8
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-03-15 12:11:18 +0100

    Added scope

--- app/models/train.rb
+++ app/models/train.rb
@@ -9,4 +9,7 @@ class Train < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
 
   has_many :carriages
+  has_many :carriages, ->(train) { order("number #{train.direction ? 'ASC' : 'DESC'}") }
+
+  scope :get_number_of_seats, ->(carriage_type, seats_type) { carriages.where(type: carriage_type).sum(seats_type) }
 end
