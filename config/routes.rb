fae5a36b49e847d890064dc72b4ed42b877df668
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-03-17 12:11:18 +0100

    Added necessary resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  resources :trains
+  resources :trains do
+    resources :carriages, shallow: true
+    resources :tickets, only: %i[new create]
+  end
+  resources :tickets, only: [:index]
+  root 'searches#show'
+
+  resource :search, only: %i[show new edit] do
+    post '/', to: 'searches#search'
+  end
   resources :railway_stations do
     member do
       post :update_position
+      post :update_time
     end
   end
   resources :routes
-  resources :carriages
   get 'welcome/index'
 
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
