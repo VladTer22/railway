bf0d34724d7a25e070f2d231599eba9ee3a6487e
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-03-25 12:11:18 +0100

    Added redirects on successful login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_welcome_index_path
+    else
+      search_path
+    end
+  end
 end
