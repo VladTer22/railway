c0512e817d76db3031fd6ab04613e0722e902043
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-03-28 12:11:18 +0100

    Altered DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,36 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :trains do
-    resources :carriages, shallow: true
-    resources :tickets, only: %i[new create]
-  end
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :tickets, only: [:index]
   root 'searches#show'
 
   resource :search, only: %i[show new edit] do
     post '/', to: 'searches#search'
   end
-  resources :railway_stations do
-    member do
-      post :update_position
-      post :update_time
-    end
+
+  resources :tickets, only: %i[show destroy]
+
+  resources :trains do
+    resources :tickets, only: %i[new create]
   end
-  resources :routes
-  get 'welcome/index'
+
+  namespace :admin do
+    resources :railway_stations do
+      member do
+        post :update_position
+        post :update_time
+      end
+    end
+
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+
+    resources :tickets, only: %i[index show edit destroy]
+
+    resources :routes
+
+    get 'welcome/index'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_101550) do
+ActiveRecord::Schema.define(version: 2021_02_25_104313) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "up_seats"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_101550) do
     t.string "unconfirmed_email"
     t.string "first_name"
     t.string "last_name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
