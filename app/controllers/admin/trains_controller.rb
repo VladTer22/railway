4da1250fb17e4cdfd47ce80d8791a5f37b5de20b
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-04-07 12:11:18 +0100

    Fixed typo

--- app/controllers/admin/trains_controller.rb
+++ app/controllers/admin/trains_controller.rb
@@ -16,7 +16,7 @@ module Admin
 
     def create
       @train = Train.new(train_params)
-      @train.current_station = @train.route.start_station
+      @train.current_station = @train.route.first_station
 
       respond_to do |format|
         if @train.save
